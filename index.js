cccb3a5aefb09a284376ed987811aff43c2756ab
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-27 17:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file
+
+function findMatching(drivers,name){
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
